93d0349083c6fda15287460ba1ec1baff1481f4d
asasanur <adityasasanur@gmail.com>
2023-06-06 12:56:54 -0400

    update test env

--- testBS.py
+++ testBS.py
@@ -8,11 +8,11 @@ import cloudscraper
 # https://www.google.com/travel/flights/booking?tfs=CBwQAhpiEgoyMDIzLTA1LTI4IiAKA0FUTBIKMjAyMy0wNS0yOBoDTEFTKgJOSzIEMTM2MSIgCgNMQVMSCjIwMjMtMDUtMjgaA1NOQSoCTksyBDE4NDlqBwgBEgNBVExyBwgBEgNTTkFAAUgBcAGCAQsI____________AZgBAg&tfu=CnhDalJJWVcxS1NUWnNRWGRrTjBWQlZIRm5jVUZDUnkwdExTMHRMUzB0ZVhOamEzVXhNRUZCUVVGQlIxSjVhM05KUjJwbE5VRkJFZzFPU3pFek5qRjhUa3N4T0RRNUdnc0kvcUVDRUFJYUExVlRSRGdjY1A2aEFnPT0SAggBIgcSBVgxZkRM
 # ke9kZe-LkdAo-RbRzK-JNdkSc pKrx3d
 # https://www.google.com/travel/flights?q=one%20way%20Flights%20to%20SNA%20from%20ATL%20on%202023-09-13
-https://www.google.com/travel/flights/search?tfs=CBwQAhooEgoyMDIzLTA5LTEzagwIAhIIL20vMDEzeXFyDAgCEggvbS8wamJyckABSAFwAYIBCwj___________8BmAEC
-click:O1htCb;gP4E0b:O1htCb;DIjhEc:YmNhJf
-BXUrOb
-https://www.google.com/travel/flights/booking?tfs=CBwQAhpqEgoyMDIzLTA5LTEzIiAKA0FUTBIKMjAyMy0wOS0xMxoDTEFTKgJOSzIEMTM2MSIeCgNMQVMSCjIwMjMtMDktMTMaA1NOQSoCTksyAjcxagwIAhIIL20vMDEzeXFyDAgCEggvbS8wamJyckABSAFwAYIBCwj___________8BmAEC&tfu=CnBDalJJWDE5eE9FbFZkRnBwWDBGQlJGZEpkMEZDUnkwdExTMHRMUzB0ZVhOaVltd3pNa0ZCUVVGQlIxSXhZbVl3UzNNek9VRkJFZ3RPU3pFek5qRjhUa3MzTVJvS0NQNVpFQUlhQTFWVFJEZ2NjUDVaEgIIASIA
-https://www.google.com/travel/flights/booking?tfs=CBwQAhpJEgoyMDIzLTA5LTEzIh8KA0FUTBIKMjAyMy0wOS0xMxoDU05BKgJETDIDNTUyagwIAhIIL20vMDEzeXFyDAgCEggvbS8wamJyckABSAFwAYIBCwj___________8BmAEC&tfu=CmxDalJJU1RWcVdIaHdXRWMxUjBWQlJEUm1WSGRDUnkwdExTMHRMUzB0ZVhOaVluUXpNRUZCUVVGQlIxSXhZM2x2UzFCTFFVRkJFZ1ZFVERVMU1ob0xDS2FjQWhBQ0dnTlZVMFE0SEhDbW5BST0SAggBIgA
+# https://www.google.com/travel/flights/search?tfs=CBwQAhooEgoyMDIzLTA5LTEzagwIAhIIL20vMDEzeXFyDAgCEggvbS8wamJyckABSAFwAYIBCwj___________8BmAEC
+# click:O1htCb;gP4E0b:O1htCb;DIjhEc:YmNhJf
+# BXUrOb
+# https://www.google.com/travel/flights/booking?tfs=CBwQAhpqEgoyMDIzLTA5LTEzIiAKA0FUTBIKMjAyMy0wOS0xMxoDTEFTKgJOSzIEMTM2MSIeCgNMQVMSCjIwMjMtMDktMTMaA1NOQSoCTksyAjcxagwIAhIIL20vMDEzeXFyDAgCEggvbS8wamJyckABSAFwAYIBCwj___________8BmAEC&tfu=CnBDalJJWDE5eE9FbFZkRnBwWDBGQlJGZEpkMEZDUnkwdExTMHRMUzB0ZVhOaVltd3pNa0ZCUVVGQlIxSXhZbVl3UzNNek9VRkJFZ3RPU3pFek5qRjhUa3MzTVJvS0NQNVpFQUlhQTFWVFJEZ2NjUDVaEgIIASIA
+# https://www.google.com/travel/flights/booking?tfs=CBwQAhpJEgoyMDIzLTA5LTEzIh8KA0FUTBIKMjAyMy0wOS0xMxoDU05BKgJETDIDNTUyagwIAhIIL20vMDEzeXFyDAgCEggvbS8wamJyckABSAFwAYIBCwj___________8BmAEC&tfu=CmxDalJJU1RWcVdIaHdXRWMxUjBWQlJEUm1WSGRDUnkwdExTMHRMUzB0ZVhOaVluUXpNRUZCUVVGQlIxSXhZM2x2UzFCTFFVRkJFZ1ZFVERVMU1ob0xDS2FjQWhBQ0dnTlZVMFE0SEhDbW5BST0SAggBIgA
 
 
 # file = open("test.html", 'r')
@@ -27,13 +27,23 @@ https://www.google.com/travel/flights/booking?tfs=CBwQAhpJEgoyMDIzLTA5LTEzIh8KA0
 #     cost = price[5][1:-1]
 #     print(day,cost)
 
-url = "https://www.google.com/travel/flights?q=one%20way%20Flights%20to%20SNA%20from%20ATL%20on%202023-09-13"
+url = "https://www.google.com/travel/flights/search?tfs=CBwQAhoeEgoyMDIzLTA2LTA2agcIARIDQVRMcgcIARIDU05BQAFIAXABggELCP___________wGYAQI&tfu=EgIIASIIEgZzVjNiZWY"
 driver = webdriver.Chrome()
 scraper = cloudscraper.create_scraper()
 driver.get(url)
-html = driver.execute_script("return document.documentElement.outerHTML")
+elements = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "yR1fYc")))
+total = min(3, len(elements))
+for i in range(total):
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "yR1fYc")))[i].click()
+    prices = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "ke9kZe-LkdAo-RbRzK-JNdkSc.pKrx3d")))
+    print(len(prices))
+    for price in prices[-60:]:
+        print(price.get_attribute("aria-label"))
+    print("NEXT")
+    driver.back()
+# html = driver.execute_script("return document.documentElement.outerHTML")
 # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "css_of_a-visible_element")))
 # html = driver.page_source
-f = open("myfile.txt", "x")
-f.write(html)
-f.close()
+# f = open("myfile.txt", "x")
+# f.write(html)
+# f.close()
