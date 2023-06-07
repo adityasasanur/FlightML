174129292add592ceaa82268ddedb88b32ea6726
asasanur <adityasasanur@gmail.com>
2023-06-07 18:05:46 -0400

    clean file directory

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,18 @@
-# Use the ARM64 Python base image
-FROM arm64v8/python:latest
+FROM public.ecr.aws/lambda/python@sha256:70b056503bffcc49eefe22897782393a984efa04be670dfd7aec1351bea6717d as build
+RUN yum install -y unzip && \
+    curl -Lo "/tmp/chromedriver.zip" "https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip" && \
+    curl -Lo "/tmp/chrome-linux.zip" "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1135561%2Fchrome-linux.zip?alt=media" && \
+    unzip /tmp/chromedriver.zip -d /opt/ && \
+    unzip /tmp/chrome-linux.zip -d /opt/
 
-# Install dependencies required for Chromium and Chrome WebDriver
-RUN apt-get update && apt-get install -y chromium-driver
-
-# Set environment variables for Chrome WebDriver
-ENV CHROME_DRIVER=/usr/lib/chromium/chromedriver
-ENV PATH=$PATH:$CHROME_DRIVER
-
-# Set environment variables for running headless Chromium
-ENV DISPLAY=:99
-
-# Set up a working directory inside the container
-WORKDIR /app
-
-# Copy requied files
-COPY requirements.txt .
-COPY utils/flightlinks.csv ./utils
-
-# Install Python dependencies
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Copy your Python application code into the container
-COPY test.py .
-
-# Set the entry point for the container
-CMD ["python", "test.py"]
+FROM public.ecr.aws/lambda/python@sha256:70b056503bffcc49eefe22897782393a984efa04be670dfd7aec1351bea6717d
+RUN yum install atk cups-libs gtk3 libXcomposite alsa-lib \
+    libXcursor libXdamage libXext libXi libXrandr libXScrnSaver \
+    libXtst pango at-spi2-atk libXt xorg-x11-server-Xvfb \
+    xorg-x11-xauth dbus-glib dbus-glib-devel -y
+RUN pip install selenium
+COPY --from=build /opt/chrome-linux /opt/chrome
+COPY --from=build /opt/chromedriver /opt/
+COPY main.py ./
+COPY flightLinks.csv ./
+CMD [ "main.handler" ]
---
+++ main.py
@@ -0,0 +1,59 @@
+import csv
+import datetime
+from tempfile import mkdtemp
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+
+def handler(event=None, context=None):
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    options.add_argument("--remote-debugging-port=9222")
+    s = Service('/opt/chromedriver')
+    driver = webdriver.Chrome(service=s, options=options)
+    links = []
+
+    with open('flightLinks.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+            links.append(row[0] + tomorrow.strftime("%Y-%m-%d"))
+
+    for url in links[:2]:
+        driver.get(url)
+        try:
+            elements = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "yR1fYc")))
+
+            total = min(1, len(elements))
+            for i in range(total):
+                try:
+                    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "yR1fYc")))[i].click()
+                except:
+                    continue
+                try:
+                    prices = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "ke9kZe-LkdAo-RbRzK-JNdkSc.pKrx3d")))
+                except:
+                    continue
+                print(len(prices))
+                for price in prices[-60:]:
+                    print(price.get_attribute("aria-label"))
+                print("NEXT")
+                driver.back()
+        except:
+            continue
+
+    driver.quit()
